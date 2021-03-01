6a296c7ef41bf415425fed6a20aef00949299fcf
vdhopteprinceton <vdhopte@princeton.edu>
2021-02-28 22:46:41 -0500

    server file

---
+++ youtubeplaylist.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask
+from flask import render_template, make_response, request, flash
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET'])
+def home():
+    html = render_template('homepage.html')
+    response = make_response(html)
+    return response
+
+@app.route('/enterartists', methods=['GET'])
+def enterartists():
+    html = render_template('enterartists.html')
+    response = make_response(html)
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
